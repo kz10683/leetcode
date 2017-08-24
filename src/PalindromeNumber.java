a272a0199bfee8d310be31d410347d9a8debf6fa
Kevin Zhang <kz10683@gmail.com>
2017-08-24 01:08:57 -0700

    Added #9 Palindrome Number

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,27 @@
+
+public class PalindromeNumber
+{
+    /**
+     * Determine whether an integer is a palindrome. Do this without extra space.
+     */
+    public boolean isPalindrome(int x)
+    {
+        int num = x;
+        int reversed = 0;
+
+        while (num > 0)
+        {
+            if (reversed > Integer.MAX_VALUE / 10)
+            {
+                return false;
+            }
+
+            int digit = num % 10;
+            num = num / 10;
+
+            reversed = (reversed * 10) + digit;
+        }
+
+        return x == reversed;
+    }
+}
