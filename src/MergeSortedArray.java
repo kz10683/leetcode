d1bdf9e4c021e660a566b09c75ef0a3a73034622
Kevin Zhang <kz10683@gmail.com>
2017-09-13 16:08:37 -0700

    Added #88 Merge Sorted Array

---
+++ src/MergeSortedArray.java
@@ -0,0 +1,44 @@
+
+public class MergeSortedArray
+{
+    /**
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
+     * one sorted array.
+     *
+     * Note: You may assume that nums1 has enough space (size that is greater or
+     * equal to m + n) to hold additional elements from nums2. The number of
+     * elements initialized in nums1 and nums2 are m and n respectively.
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        // Starting from the end of nums1, start setting largest values from nums1 and
+        // nums2.
+        while (i >= 0 && j >= 0)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        // Set leftover values, if any
+        while (j >= 0)
+        {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
