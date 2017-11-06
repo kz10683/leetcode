0e3f218cde63a98da36e2caa9e1044afcfadf100
Kevin Zhang <kz10683@gmail.com>
2017-11-06 13:47:19 -0800

    Added #168 Excel Sheet Column Title

---
+++ src/ExcelSheetColumnTitle.java
@@ -0,0 +1,35 @@
+public class ExcelSheetColumnTitle
+{
+    /**
+     * Given a positive integer, return its corresponding column title as appear in
+     * an Excel sheet.
+     *
+     * For example:
+     *
+     * 1 -> A, 2 -> B, 3 -> C, ... 26 -> Z, 27 -> AA, 28 -> AB
+     */
+    public static String convertToTitle(int n)
+    {
+        String title = "";
+        while (n > 0)
+        {
+            // We know there are 26-chars in the alphabet, so we mod by this to retrieve the
+            // offset. This offset is used to determine the appropriate char to append from
+            // 'A'.
+            int offset = (n - 1) % 26;
+
+            char c = (char) ('A' + offset);
+            title = c + title;
+
+            n = (n - 1) / 26;
+        }
+        return title;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(convertToTitle(1));
+        System.out.println(convertToTitle(26));
+        System.out.println(convertToTitle(28));
+    }
+}
