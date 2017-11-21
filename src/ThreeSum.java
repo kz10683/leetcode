3f68cd960b3ef9c3ac9fcfda0cbb6f9eda76affb
Kevin Zhang <kz10683@gmail.com>
2017-11-21 15:10:21 -0800

    Added #15 3Sum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -60,21 +60,4 @@ public class ThreeSum
         return new ArrayList<List<Integer>>(set);
     }
 
-    public static void main(String[] args)
-    {
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-
-        List<Integer> list2 = new ArrayList<Integer>();
-        list2.add(1);
-        list2.add(2);
-
-        Set<List<Integer>> set = new HashSet<List<Integer>>();
-        set.add(list);
-        set.add(list2);
-
-        System.out.println(set.size());
-    }
-
 }
