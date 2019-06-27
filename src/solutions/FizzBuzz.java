7e231ff284f94b2cda229bd92e3b25f8a330ba03
Kevin Zhang <kz10683@gmail.com>
2019-06-26 23:49:28 -0700

    Updated javadoc comments for #412 FizzBuzz

--- src/solutions/FizzBuzz.java
+++ src/solutions/FizzBuzz.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class FizzBuzz
 {
     /**
-     * 412. Write a program that outputs the string representation of numbers from 1 to n. But for multiples of three it should
-     * output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of
-     * both three and five output “FizzBuzz”.
+     * 412. Write a program that outputs the string representation of numbers from 1 to n. But for multiples of three it
+     * should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are
+     * multiples of both three and five output “FizzBuzz”.
      */
     public static List<String> fizzBuzz(int n)
     {
