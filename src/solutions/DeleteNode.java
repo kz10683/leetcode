51489c8c8bb3b7c767c53507723a763221415493
Kevin Zhang <kz10683@gmail.com>
2019-07-23 23:23:32 -0700

    Updated javadoc for #237

--- src/solutions/DeleteNode.java
+++ src/solutions/DeleteNode.java
@@ -10,15 +10,15 @@ public class DeleteNode
      * <p>
      * Input: head = [4,5,1,9], node = 5
      * Output: [4,1,9]
-     * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+     * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling
+     * your function.
      * <p>
      * Example 2:
      * <p>
      * Input: head = [4,5,1,9], node = 1
      * Output: [4,5,9]
-     * Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
-     * <p>
-     * <p>
+     * Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling
+     * your function.
      * <p>
      * Note:
      * <p>
